55361150141d5a064eef7536ccb91dabac035f64
HammadButt786 <81991271+HammadButt786@users.noreply.github.com>
2021-04-06 14:33:11 +0500

    exam

---
+++ web.h
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ORIC || IIUI</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <div class = "image" >
+        <img class="img1" src="image11.JPG" alt="">
+        <img class= "img2" src="image12.JPG" alt="">
+        <img class="img3" src="image13.JPG" alt="">
+    </div>
+
+    
+   <div class="menu">
+            <a class="active" href="">About</a>
+            <a href="">Funding Opurtunities</a>
+            <a href="">External Funding</a>
+            <a href="">IIUI Funding</a>
+            <a href="">Downloads</a>
+            <a href="">Online Research Database</a>
+            <a href="">Events</a>
+            <a href="">ORIC staff</a>
+  </div>
+
+  <table id="myTable" class="table table-bordered">
+    <thead style="text-align:center">
+                             <tr style="color:green; text-align:center; font-size:20px"><th colspan="7" style="text-align:center" >Approved Travel Grant Cases</th></tr>
+                            <tr><th style="text-align:center">Sr #</th>
+                              <th style="text-align:center">Applicant Name</th>
+                              <th style="text-align:center">Designation & Faculty</th>
+                                  
+                                   <th style="text-align:center">Research Paper Title</th>
+                                   <th style="text-align:center">Title of Conference</th>
+                                   <th style="text-align:center">Dates & Country</th>
+                                   <th style="text-align:center">Amount Approved by HEC</th>
+                                 
+                                </tr>
+                              
+                              </thead>
+                              <tbody>
+                                                                  <tr>
+                                  <td>1</td>
+                                        <td>Dr. Riffat Asim Pasha</td>
+                                      <td>Assistant Professor, Engineering and Technology</td>
+                                        <td>Temperature Effects on Impact Energy of Tungsten Alloy</td>
+                                      <td>Materials Science & Technology Conference & Exhitition</td>
+                                      <td>United States</td>
+                                      <td>Rs 257210/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>2</td>
+                                        <td>Mr. Jawwad Ahmed</td>
+                                      <td>PhD Scholar, Management Sciences</td>
+                                        <td>Job Satisfaction of Teaching Faculty Working in Universities of Pakistan</td>
+                                      <td>UCTI Business & Management Conference</td>
+                                      <td>Malaysia</td>
+                                      <td>Rs 113638/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>3</td>
+                                        <td>Mr. Zahid Mehmood</td>
+                                      <td>Assistant Director, Other</td>
+                                        <td>Battery Effective Time Suncronization in Wireless Sensor Networks</td>
+                                      <td>3rd Intel. Conference on Machine Learning & Computing</td>
+                                      <td>Singapore</td>
+                                      <td>Rs 183040/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>4</td>
+                                        <td>Muhammad Umer</td>
+                                      <td>PhD Scholar, Management Sciences</td>
+                                        <td>Facing Challenges in Global Environment; A Study of International Business Transformation</td>
+                                      <td>2nd International conference on Busines & Economic research 2nd ICBER</td>
+                                      <td>Malaysia</td>
+                                      <td>Rs 145950/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>5</td>
+                                        <td>Syed Husnain Abbas Naqvi</td>
+                                      <td>Assistant Professor, Basic and Applied Sciences</td>
+                                        <td>Energy Efficient MISO Collaborative Communication System AWGN and Rayleigh Fading Channel</td>
+                                      <td>IEEE Global Communications Conference</td>
+                                      <td>United States</td>
+                                      <td>Rs 285616/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>6</td>
+                                        <td>Mr. Afzaal Ali</td>
+                                      <td>PhD Scholar, Management Sciences</td>
+                                        <td>Relationship of Environmental Concerns and Pro-Environmental Behaviour with Moderating effect of Religiosity</td>
+                                      <td>14th International Business Research Conference</td>
+                                      <td>United Arab Emirates</td>
+                                      <td>Rs 147779/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>7</td>
+                                        <td>Dr. S.M. Bhutta</td>
+                                      <td>Professor, Engineering and Technology</td>
+                                        <td>Ananlysis of Energy Crisis in Pakistan & Remedial Measures Proposed</td>
+                                      <td>8th asia pacific conference on sustainable energy and environmental technologies</td>
+                                      <td>Australia</td>
+                                      <td>Rs 197319/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>8</td>
+                                        <td>Mr. Muhammad Abbass</td>
+                                      <td>PhD Scholar, Management Sciences</td>
+                                        <td>International Enterpreunership Congress </td>
+                                      <td>International Enterpreunership Congress</td>
+                                      <td>Turkey</td>
+                                      <td>Rs 127160/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>9</td>
+                                        <td>Dr. Syed Zulfiqar Ali Shah</td>
+                                      <td>Assistant Professor, Management Sciences</td>
+                                        <td>Impact of Leverage on Performance of Firms, Evidence from Pakistan</td>
+                                      <td>The International Business, Finance & Economics Research Conference</td>
+                                      <td>United States</td>
+                                      <td>Rs 273790/-</td>
+                                  </tr>
+                                                                      <tr>
+                                  <td>10</td>
+                                        <td>Ms. Qurat Ul Ain Khair</td>
+                                      <td>PhD Scholar, Management Sciences</td>
+                                        <td>The relationship between Mosculine /feminine culteral deministion of hofstede and legal gender parity laws for workers across different nations</td>
+                                      <td>Global Conference on Innovation in Management & Doctoral symposium</td>
+                                      <td>United Kingdom</td>
+                                      <td>Rs 180075/-</td>
+                                  </tr>
+                                                                      <tr>
+                                                                                               
+                              </tbody>
+                 <tfoot>
+                                   <tr>
+                                  <td colspan="7" style="text-align:center; font-size: 20px">
+                                      <b >
+                                      Total Approved Amount: 54.15M                                        </b>
+                                      </td>
+                                  </tr>
+                              </tfoot>
+                           
+                            </table>
+                      
+    
+</body>
+</html>
